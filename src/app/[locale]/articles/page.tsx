08e67a6fb77ed7861229d51c8dc4163acaee0195
Carlos Vera <[carlos.veradiago@gmail.com])>
2026-08-03 12:59:44 +0200

    Ensure all page files have content

---
+++ src/app/[locale]/articles/page.tsx
@@ -0,0 +1,7 @@
+export default function ArticlesPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Articles</h1>
+    </main>
+  );
+}
